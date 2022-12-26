691a47d8573185ae7c505b8b0be4b9d0e2c317a2
Raja <Raja@LAPTOP-TFS594LG>
2022-12-26 20:46:33 +0530

    shopping-cart-app-spring-1st-project

---
+++ shopping-cart-app/src/main/java/com/shopping/cart/app/repository/EmployeeRepository.java
@@ -0,0 +1,30 @@
+package com.shopping.cart.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.shopping.cart.app.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+
+	@Query("select e from Employee e where e.firstName = ?1 and e.lastName = ?2")
+	List<Employee> findByFirstNameAndLastName(String firstName, String lastName);
+	
+	@Query("select e from Employee e where e.firstName = ?1 and e.email = ?2")
+	List<Employee> findByFirstNameAndEmail(String firstName, String email);
+	
+	@Query("select e from Employee e where e.jobTitle= ?1")
+	List<Employee> findByJobTitle(String jobTitle);
+	
+	@Query("from Employee order by lastName desc")
+	List<Employee> findAllEmployeeOrderByCreditLimit();
+	
+	@Query("select e from Employee e where e.office = ?1")
+	List<Employee> findByOfficeCode(int officeCode);
+
+}

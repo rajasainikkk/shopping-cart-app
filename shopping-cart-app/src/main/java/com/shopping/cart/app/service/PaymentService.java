7e7a0fd40239c08c51386e6ed46399ddde115a75
Raja <Raja@LAPTOP-TFS594LG>
2022-12-27 13:25:03 +0530

    service and serviceimpl

---
+++ shopping-cart-app/src/main/java/com/shopping/cart/app/service/PaymentService.java
@@ -0,0 +1,27 @@
+package com.shopping.cart.app.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.shopping.cart.app.model.Payment;
+
+public interface PaymentService {
+	Optional<Payment> findById(long id);
+
+	Payment save(Payment payment);
+	
+	void delete(long id);
+
+	List<Payment> findAll();
+
+	List<Payment> findByCustomerNumber(long customerNumber);
+	
+	List<Payment> findAllPaymentByAmount();
+	
+	List<Payment> findByAmountLessThan(double amount);
+	
+	List<Payment> findByAmountGreaterThan(double amount);
+
+	
+	
+}
